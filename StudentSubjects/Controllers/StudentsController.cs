5719ed4ab1ea0e6fd036e1ae560b044b5e5a1144
CsharpSyntraWestKortrijk <61596529+CSharpSyntraWest@users.noreply.github.com>
2021-02-11 15:01:32 +0100

    Add files via upload

---
+++ StudentSubjects/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSubjects.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        public StudentsController(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            var students = _repository.Student.GetAllStudents(false);//query is sneller wanneer trackchanges af staat
+            return Ok(students); //status OK = code 200       
+        }
+
+
+    }
+}
